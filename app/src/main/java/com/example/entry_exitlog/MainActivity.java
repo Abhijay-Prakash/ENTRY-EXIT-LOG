d780d745fa1b13919bbbb9985abdc42cb8b9de8a
Abhijay-Prakash <Abhijay-Prakash>
2024-06-28 11:14:07 +0530

    Login Page

--- app/src/main/java/com/example/entry_exitlog/MainActivity.java
+++ app/src/main/java/com/example/entry_exitlog/MainActivity.java
@@ -1,19 +1,38 @@
 package com.example.entry_exitlog;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText d1,d2;
+    AppCompatButton b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        d1=(EditText) findViewById(R.id.ed1);
+        d2=(EditText) findViewById(R.id.ed2);
+        b1=(AppCompatButton) findViewById(R.id.bt1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1,s2;
+                s1=d1.getText().toString();
+                s2=d2.getText().toString();
+                Toast.makeText(getApplicationContext(), s1+""+s2, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
