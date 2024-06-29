e16c25a10e0e0273db72654f7f5e8e30cdfc8ea5
Abhijay-Prakash <Abhijay-Prakash>
2024-06-29 14:09:40 +0530

    API INTEGRATION

--- app/src/main/java/com/example/entry_exitlog/LogScreen.java
+++ app/src/main/java/com/example/entry_exitlog/LogScreen.java
@@ -14,12 +14,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 import java.time.LocalTime;
 
 public class LogScreen extends AppCompatActivity {
 
     EditText d1,d2,d3,d4;
     AppCompatButton b1,b2;
+    String apiUrl="http://10.0.4.16:3000/api/students";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,9 +56,45 @@ public class LogScreen extends AppCompatActivity {
                     s4=d4.getText().toString();
                     LocalTime currentTime=LocalTime.now();
                     String s5=currentTime.toString();
-                    Toast.makeText(getApplicationContext(),"Login Time:"+s5, Toast.LENGTH_LONG).show();
 
-                    Toast.makeText(getApplicationContext(),s1+s2+s3+s4,Toast.LENGTH_LONG).show();
+
+                    //Creating JSON object and sending it via key-value pair
+                    JSONObject stud=new JSONObject();
+                    try{
+                        stud.put("name",s1);
+                        stud.put("admission_number",s2);
+                        stud.put("system_number",s3);
+                        stud.put("department",s4);
+                    }
+                    catch (Exception e){
+                        Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_LONG).show();
+                    }
+
+
+                    //JSON object request creation
+                    JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(
+                            Request.Method.POST,
+                            apiUrl,
+                            stud,
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    Toast.makeText(getApplicationContext(), "Added Successfully", Toast.LENGTH_SHORT).show();
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Toast.makeText(getApplicationContext(), "Something went wrong, Try Again", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                    );
+
+
+                    //Request Queue
+                    RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+                    requestQueue.add(jsonObjectRequest);
+
 
                 }
                 catch (Exception e){
